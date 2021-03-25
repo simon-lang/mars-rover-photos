2b801fbc122a555da417d0ed6101d1969d0e2b7c
Simon Lang <simon.lang@lens.org>
2021-03-25 12:05:44 +1000

    Remove sample content

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { observer  } from 'mobx-react';
+
+import Timer from './store/timer'
+
+const myTimer = new Timer()
+
+const TimerView = observer(({ timer }) => (
+    <button onClick={() => timer.reset()}>Seconds passed: {timer.secondsPassed}</button>
+))
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <TimerView timer={myTimer} />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +24,10 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
+
+
+// Update the 'Seconds passed: X' text every second.
+setInterval(() => {
+    myTimer.increase()
+}, 1000)
