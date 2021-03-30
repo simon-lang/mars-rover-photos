c4fcc07eb8441c2fdd4e6a7255f3d09243eb2550
Simon Lang <simon.lang@lens.org>
2021-03-30 20:26:44 +1000

    Update photo service to TS and use dayjs instead of moment

--- src/services/photo-service.ts
+++ src/services/photo-service.ts
@@ -1,55 +1,28 @@
 import axios from 'axios'
+import dayjs from 'dayjs'
 
-const API_URL: string = 'https://api.nasa.gov/mars-photos/api/v1/';
-const API_KEY: string = 'wwYXJBLASfX4wNrbLfEetDdx6U3EbRSm1Lx93DGa';
+const API_URL: string = 'https://api.nasa.gov/mars-photos/api/v1/'
+const API_KEY: string = 'wwYXJBLASfX4wNrbLfEetDdx6U3EbRSm1Lx93DGa'
 
-// if (location.host === 'captainclam.github.io') {
-//     API_KEY = 'P1YSIZOzLqwxUbFxX2Gvj4vNu9Rgs7qjjpEkld3A';
-// } else {
-//     API_KEY = 'wwYXJBLASfX4wNrbLfEetDdx6U3EbRSm1Lx93DGa';
-// }
-
-export const getPhotos = async function(filter) {
-    var url;
-    url = API_URL + 'rovers/' + filter.rover.toLowerCase() + '/photos' + '?camera=' + filter.camera;
+export const getPhotos = async (filter) => {
+    let url: string = API_URL + 'rovers/' + filter.rover.toLowerCase() + '/photos?camera=' + filter.camera
     if (filter.sol != null) {
-        url += '&sol=' + filter.sol;
+        url += '&sol=' + filter.sol
     } else {
-        // url += '&earth_date=' + moment(filter.earth_date).format('YYYY-M-D');
+        url += '&earth_date=' + dayjs(filter.earth_date).format('YYYY-M-D')
     }
-    url += '&api_key=' + (API_KEY || 'DEMO_KEY');
+    url += `&api_key=${ API_KEY || 'DEMO_KEY' }`
     return axios({
         method: 'GET',
         url: url
-    });
+    })
 }
 
-
-const getManifest = function(rover) {
+export const getManifest = function(rover: string) {
     return axios({
         method: 'GET',
         url: API_URL + 'manifests/' + rover.toLowerCase() + '?api_key=' + (API_KEY || 'DEMO_KEY')
     }).then(function({data}) {
-        return data.photo_manifest;
-    });
-}
-
-const encode = function(photos) {
-    photos = photos.map(function(photo) {
-        return {
-            img_src: photo.img_src,
-            earth_date: photo.earth_date,
-            rover: {
-                name: photo.rover.name
-            },
-            camera: {
-                name: photo.camera.name
-            }
-        };
-    });
-    return window.btoa(JSON.stringify(photos));
-}
-
-const decode = function(str) {
-    return JSON.parse(window.atob(str));
+        return data.photo_manifest
+    })
 }
